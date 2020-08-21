d5eb0438b3983a0e15e77f20fa8b358b25574c8e
akash <akash@akash>
2020-08-21 10:12:06 +0530

    Deactivate scheduler and email in student entity.

--- basic-project-crud/src/main/java/basicprojectcrud/BasicProjectCrudApplication.java
+++ basic-project-crud/src/main/java/basicprojectcrud/BasicProjectCrudApplication.java
@@ -2,10 +2,9 @@ package basicprojectcrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling //If you uncomment it then HiiHelloScheduler Service will run
+//@EnableScheduling //If you uncomment it then HiiHelloScheduler Service will run
 public class BasicProjectCrudApplication {
 
 	public static void main(String[] args) {
--- basic-project-crud/src/main/java/basicprojectcrud/entity/Student.java
+++ basic-project-crud/src/main/java/basicprojectcrud/entity/Student.java
@@ -1,13 +1,21 @@
 package basicprojectcrud.entity;
 
+import java.time.LocalDate;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -22,15 +30,24 @@ import lombok.Setter;
 @AllArgsConstructor
 public class Student {
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "STU_ID_SEQ")
+	@SequenceGenerator(name = "STU_ID_SEQ", sequenceName = "STUDENT_ID_SEQ", initialValue=1)
 	@Column(name = "STUDENT_ID", updatable = false, nullable = false)
 	private Long studentId;
 
 	@Column(name = "NAME")
-	@NotBlank(message = "Name must be non null.")
+	@NotBlank(message = "Name is required")
 	private String name;
 
 	@Column(name = "PHONE_NUMBER")
 	@Pattern(regexp = "(^$|[0-9]{10})")
 	private String phoneNumber;
+	
+	@NotEmpty(message = "Email is required")
+	@Email(regexp = "^[\\w-\\+]+(\\.[\\w]+)*@[\\w-]+(\\.[\\w]+)*(\\.[a-z]{2,})$",message = "Email should be valid")
+    private String email;
+	
+	@Past
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate birthday;
 }
